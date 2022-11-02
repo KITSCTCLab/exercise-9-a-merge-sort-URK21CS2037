07942b70c74e990e7e6ec30f4d11bd328bda075b
URK21CS2037 <109648094+URK21CS2037@users.noreply.github.com>
2022-11-02 13:36:19 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -42,5 +42,5 @@ for item in input_data.split(', '):
     data.append(int(item))
   elif item.lstrip("-").isnumeric():
     data.append(int(item))
-merge_sort(data)
+merge_sort(data,l,r)
 print(data)
